b8b81a08e15e370989bb2cbfbea15d90db444c5d
shiyawei <shiyawei@weibopay.com>
2018-12-26 16:17:53 +0800

    初始化

---
+++ src/main/java/com/gittest/common/Log.java
@@ -0,0 +1,22 @@
+package com.gittest.common;
+
+import java.lang.annotation.*;
+
+/**
+ * <p>todo</p>
+ *
+ * @author syw
+ * @version $Id: Log, v 0.1 2018/12/26 15:40 syw Exp $
+ */
+@Target({ ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface Log {
+
+    /** 模块 */
+    String title() default "";
+
+    /** 功能 */
+    String action() default "";
+
+}
